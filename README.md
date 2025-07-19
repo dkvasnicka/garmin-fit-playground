bac90978fb8a821bd8227420e50090abcf3ef856
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2025-07-19 16:03:39 +0200

    first bits

---
+++ README.md
@@ -0,0 +1,14 @@
+garmin-fit-playground
+=====================
+
+Rust playground for doing analysis on data from my Garmin Edge that I miss in existing
+cycling/athlete support software.
+
+## Currently implemented features
+
+* *Rear gear weighted median value* -- for finding out what rear gear is the most used one throughout your ride so you can
+optimize your 1x chainring size if you have more of them to swap out.
+
+## Howto
+
+Just run `cargo run -- /path/to/activity.fit`
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use fitparser::{self, Value};
+use itertools::Itertools;
+use std::{env, fs::File};
+use weighted_median::{Data, weighted_median};
+
+struct InGear {
+    pub gear: u8,
+    pub duration: i64,
+}
+
+impl Data for InGear {
+    fn get_value(&self) -> f64 {
+        self.gear as f64
+    }
+
+    fn get_weight(&self) -> f64 {
+        self.duration as f64
+    }
+}
+
+fn main() {
+    println!(
+        "Parsing FIT files using Profile version: {:?}",
+        fitparser::profile::VERSION
+    );
+    let args: Vec<String> = env::args().collect();
+    let mut fp = File::open(&args[1]).unwrap();
+
+    let mut rear_gears = fitparser::from_reader(&mut fp)
+        .unwrap()
+        .into_iter()
+        .filter_map(|data| {
+            let gear_change_data = data
+                .fields()
+                .iter()
+                .find(|f| f.name() == "gear_change_data");
+            if let Some(gcd_val) = gear_change_data {
+                let ts_field = data
+                    .fields()
+                    .iter()
+                    .find(|f| f.name() == "timestamp")
+                    .unwrap();
+
+                // println!("{:?}", data);
+                let gear_nums = match gcd_val.value() {
+                    Value::UInt32(v) => v.swap_bytes().to_ne_bytes(),
+                    _ => panic!("unsupported gear data"),
+                };
+                let timestamp = match ts_field.value() {
+                    Value::Timestamp(d) => d.timestamp(),
+                    _ => panic!("got a gear change event without timestamp"),
+                };
+
+                Some((timestamp, gear_nums[3]))
+            } else {
+                None
+            }
+        })
+        .dedup_by(|l, r| l.1 == r.1)
+        .tuple_windows()
+        .map(|(prev, next)| InGear {
+            gear: prev.1,
+            duration: next.0 - prev.0,
+        })
+        .collect_vec();
+
+    println!(
+        "rear gear weighted median: {:?}",
+        weighted_median(rear_gears.as_mut_slice())
+    );
+}
